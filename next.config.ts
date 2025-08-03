f9c4f4ae30cb830f1376b74fdb3791a29b958332
Abhijeet Kadam <kadamabhi1881@gmail.com>
2025-08-03 23:19:25 +0530

    Made configuration for model rendering

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/models/:file*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'model/gltf-binary',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
